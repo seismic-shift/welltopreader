e73368e89a17cf22a95ca8287c1260f007c18453
nickk14 <44319910+nickk14@users.noreply.github.com>
2019-09-17 11:39:03 +0100

    Added from_txt constructor method

--- reader.py
+++ reader.py
@@ -1,5 +1,32 @@
+import re
 
-with open('header.txt', 'r') as f:
-    lines = f.readlines()
+class Well:
 
-print(lines)
+    def __init__(self):
+        return
+
+    def format_string(self, str_in):
+        return re.sub('\W+', '_', str_in).lower()
+
+    @classmethod
+    def from_txt(cls, pth, sep=':'):
+
+        with open(pth, 'r') as f:
+            lines = f.readlines()
+
+        cls.attrs = {}
+        for line in lines:
+            rtn = line.replace(' ', '').replace('#', '').strip().split(sep)
+
+            if len(rtn) == 2:
+                
+                setattr(cls, rtn[0], rtn[1])
+                cls.attrs[rtn[0]] = rtn[1]
+
+        return cls
+
+w1 = Well.from_txt('header.txt')
+
+print(w1)
+
+print(w1.attrs.keys())
